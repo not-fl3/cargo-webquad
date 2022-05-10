0b6986b94b8857cd19ab88f3baafdedef0aacd4a
Fedor Logachev <not.fl3@gmail.com>
2022-05-10 13:40:34 -0500

    Update cargo version

--- src/main.rs
+++ src/main.rs
@@ -94,6 +94,7 @@ fn main() {
         ),
         target_rustdoc_args: None,
         target_rustc_args: None,
+        target_rustc_crate_types: None,
         local_rustdoc_args: None,
         rustdoc_document_private_items: false,
         honor_rust_version: false,
